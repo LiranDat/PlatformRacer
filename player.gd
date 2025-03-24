798846f585906667b546bfe676d6459f1e87e2a7
reyojne-aeki <jo8472@proton.me>
2025-03-24 21:02:08 +0100

    Habe mal ein bisschen den player rumgedingst

--- player.gd
+++ player.gd
@@ -3,14 +3,14 @@ extends CharacterBody3D
 func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("P1Jump"):
-		velocity.y = 10.0
+		velocity.y = 5.0
 	velocity.y += get_gravity().y*delta
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var verticalDirection := Input.get_axis("P1Left", "P1Right")
-	velocity.x = move_toward(velocity.x,verticalDirection*10.0, 10.0)
+	velocity.x = move_toward(velocity.x,verticalDirection*10.0, 0.5)
 	var horizontalDireciton = Input.get_axis("P1Up", "P1Down")
-	velocity.z = move_toward(velocity.z,horizontalDireciton*10.0, 10.0)
+	velocity.z = move_toward(velocity.z,horizontalDireciton*10.0, 0.5)
 	#Wind in between player and body, affect by wind
 	move_and_slide()
 
