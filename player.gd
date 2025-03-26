883b8d42470a38e95d11939e8581b4bf98006ad3
Liran <lirandattner@googlemail.com>
2025-03-26 20:47:32 +0100

    Rudimentary Control

--- player.gd
+++ player.gd
@@ -2,14 +2,15 @@ extends CharacterBody3D
 
 func _physics_process(delta: float) -> void:
 	# Handle jump.
-	if Input.is_action_just_pressed("up"):
+	if Input.is_action_just_pressed("P1Jump"):
 		velocity.y = 10.0
-	if Input.is_action_pressed("up"):
-		velocity.y -= get_gravity().y/2.0*delta
+	velocity.y += get_gravity().y*delta
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	velocity.x = move_toward(velocity.x,direction*10.0, 10.0)
+	var verticalDirection := Input.get_axis("P1Left", "P1Right")
+	velocity.x = move_toward(velocity.x,verticalDirection*10.0, 10.0)
+	var horizontalDireciton = Input.get_axis("P1Up", "P1Down")
+	velocity.z = move_toward(velocity.z,horizontalDireciton*10.0, 10.0)
 	#Wind in between player and body, affect by wind
 	move_and_slide()
 
